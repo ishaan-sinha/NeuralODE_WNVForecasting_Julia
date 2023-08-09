74a175d6e62e7e8a616b763f2bebb9b92c8ee8b8
ishaan <ishaan.mohan.sinha@gmail.com>
2023-08-09 19:04:05 -0400

    diffeq_param

--- model-zooTutorial/ode_paramfit.jl
+++ model-zooTutorial/ode_paramfit.jl
@@ -24,4 +24,45 @@ sol_ode = solve(prob_ode, Tsit5())
 # Plot the solution
 using Plots
 plot(sol_ode)
-savefig("LV_ode.png")
+
+# Create a solution (prediction) for a given starting point u0 and set of
+# parameters p
+function predict_adjoint(p)
+    return Array(concrete_solve(prob_ode, Tsit5(), u0, p, saveat = tsteps))
+  end
+
+function loss_adjoint(p)
+    prediction = predict_adjoint(p)
+    loss = sum(abs2, x-(x^2-2) for x in prediction)
+    return loss, prediction
+end
+
+# Callback function to observe training
+list_plots = []
+iter = 0
+callback = function (p, l, pred)
+  global iter
+
+  if iter == 0
+    list_plots = []
+  end
+  iter += 1
+
+  display(l)
+
+  # using `remake` to re-create our `prob` with current parameters `p`
+  remade_solution = solve(remake(prob_ode, p = p), Tsit5(), saveat = tsteps)
+  plt = plot(remade_solution, ylim = (0, 6))
+
+  push!(list_plots, plt)
+  display(plt)
+
+  # Tell sciml_train to not halt the optimization. If return true, then
+  # optimization stops.
+  return false
+end
+
+result_ode = DiffEqFlux.sciml_train(loss_adjoint, p,
+                                    BFGS(initial_stepnorm = 0.0001),
+                                    cb = callback)
+
